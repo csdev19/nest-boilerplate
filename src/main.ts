1224bb2a5396be21bc3d5e02f19cafe77ba78ed2
cristiandev <cristiansotomayor.dev@gmail.com>
2021-12-02 10:40:01 -0500

    main file using nest fastify application

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Investigation example')
+    .setDescription('The investigation API description')
+    .setVersion('1.0')
+    .addTag('investigate')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
