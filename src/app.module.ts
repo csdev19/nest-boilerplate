b8366c41c74d5a143a06a1f09587539d0b56ce23
cristiandev <cristiansotomayor.dev@gmail.com>
2021-12-02 10:41:56 -0500

    app module with typeorm, config module

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { NoteModule } from './modules/note/note.module';
+import { Note } from './modules/note/note.entity';
+import { Account } from './modules/auth/entities/account.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_DB_HOST,
+      port: +process.env.PG_DB_PORT,
+      username: process.env.PG_DB_USERNAME,
+      password: process.env.PG_DB_PASSWORD,
+      database: process.env.PG_DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [Account, Note],
+    }),
+    AuthModule,
+    NoteModule,
+  ],
+})
+export class AppModule {}
