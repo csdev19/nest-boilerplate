7f04a1c58fdebffcbb01b796a9629a0b054e46d9
cristiandev <cristiansotomayor.dev@gmail.com>
2021-12-03 01:04:59 -0500

    note service

---
+++ src/modules/note/note.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateNoteDto } from './dtos/create-note.dto';
+import { UpdateNoteDto } from './dtos/update-note.dto';
+import { Note } from './note.entity';
+
+@Injectable()
+export class NoteService {
+  constructor(
+    @InjectRepository(Note)
+    private readonly notesRepository: Repository<Note>,
+  ) {}
+
+  findAll() {
+    return this.notesRepository.find();
+  }
+
+  findById(id: string) {
+    return this.notesRepository.findOne(id);
+  }
+
+  create(noteDto: CreateNoteDto) {
+    const note = new Note();
+    note.title = noteDto.title;
+    note.content = noteDto.content;
+    return this.notesRepository.save(note);
+  }
+
+  update(id, noteDto: UpdateNoteDto) {
+    return this.notesRepository.update(id, { ...noteDto });
+  }
+
+  async delete(id: string) {
+    const { affected } = await this.notesRepository.delete(id);
+    return affected > 0;
+  }
+}
