a3c8d63822d6e8e623c40a7cbf358d4511eae8d7
cristiandev <cristiansotomayor.dev@gmail.com>
2021-12-03 01:04:42 -0500

    creating the note controller

---
+++ src/modules/note/note.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthenticationGuard } from '../authentication/guards/jwt-authentication.guard';
+import { CreateNoteDto } from './dtos/create-note.dto';
+import { UpdateNoteDto } from './dtos/update-note.dto';
+import { NoteService } from './note.service';
+
+@Controller('notes')
+export class NoteController {
+  constructor(private noteService: NoteService) {}
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Post()
+  create(@Body() noteDto: CreateNoteDto) {
+    return this.noteService.create(noteDto);
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Get()
+  findAll() {
+    return this.noteService.findAll();
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.noteService.findById(id);
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Put(':id')
+  update(@Param('id') id: string, @Body() noteDto: UpdateNoteDto) {
+    return this.noteService.update(id, noteDto);
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.noteService.delete(id);
+  }
+}
