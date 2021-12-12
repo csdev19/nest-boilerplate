eebd4e4683b349680610d94e548b87b811d1b18c
cristiandev <cristiansotomayor.dev@gmail.com>
2021-12-12 01:32:08 -0500

    update path

--- src/modules/accounts/services/accounts.service.ts
+++ src/modules/accounts/services/accounts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateAccountDto } from './dtos/create-account.dto';
-import { Account } from './entities/account.entity';
+import { CreateAccountDto } from '../dtos/create-account.dto';
+import { Account } from '../entities/account.entity';
 
 @Injectable()
 export class AccountsService {
