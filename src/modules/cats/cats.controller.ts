8e180c09da657cf9776863a2f3eb49573bad46ed
cristiandev <cristiansotomayor.dev@gmail.com>
2021-12-21 23:36:51 -0500

    new cat module

---
+++ src/modules/cats/cats.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+// import { Roles } from '../common/decorators/roles.decorator';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  // @Roles('admin')
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id')
+    id: number,
+  ) {
+    // get by ID logic
+  }
+}
